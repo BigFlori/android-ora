6b4eec8f8b4981c4345d44c605b610c14780eb86
Thurneer <florian00m14@gmail.com>
2021-10-09 11:25:12 +0200

    Update

--- app/src/main/java/com/example/firstapp/MainActivity.kt
+++ app/src/main/java/com/example/firstapp/MainActivity.kt
@@ -1,15 +1,39 @@
 package com.example.firstapp
 
 import android.os.Bundle
+import android.view.MenuItem
 import android.view.View
 import android.widget.Button
 import android.widget.TextView
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
+import androidx.fragment.app.Fragment
+import com.example.firstapp.fragments.AlarmFragment
+import com.example.firstapp.fragments.StopperFragment
+import com.example.firstapp.fragments.TimerFragment
+import com.google.android.material.bottomnavigation.BottomNavigationView
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : AppCompatActivity(), BottomNavigationView.OnNavigationItemSelectedListener {
+    lateinit var bottomNavigationView: BottomNavigationView
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        bottomNavigationView = this.findViewById(R.id.bottomNavigation)
+        this.supportFragmentManager.beginTransaction().replace(R.id.main_container, AlarmFragment()).commit()
+        bottomNavigationView.selectedItemId = R.id.nav_alarm
+        bottomNavigationView.setOnNavigationItemSelectedListener(this)
+    }
+
+    override fun onNavigationItemSelected(item: MenuItem): Boolean {
+        lateinit var desiredFragment: Fragment
+        when(item.itemId) {
+            R.id.nav_alarm -> desiredFragment = AlarmFragment()
+            R.id.nav_stopper -> desiredFragment = StopperFragment()
+            R.id.nav_timer -> desiredFragment = TimerFragment()
+        }
+        this.supportFragmentManager.beginTransaction().replace(R.id.main_container, desiredFragment).commit()
+        return true
     }
 }
